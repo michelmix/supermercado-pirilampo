52fc401ebbcdc4bb68c4269efc32415609342e0b
Michel Barbosa <michelbarbosa@outlook.com>
2024-05-02 11:08:10 -0300

    Cria função para retornar todos os nomes dos produtos do estoque em um array

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -1,7 +1,27 @@
 const stockProducts = require('./data.json');
 
+// const getUniqueProductsName = () => {
+//   let productsName = [];
+
+//   for (let index = 0; index < stockProducts.length; index++) {
+//     let product = stockProducts[index].productName
+//     productsName.push(product);
+//   }
+//   return productsName;
+// };
+
+// console.log(getUniqueProductsName());
+
 const getUniqueProductsName = () => {
-  // Desenvolva seu código dentro dessa função...
+  let productsName = [];
+
+  for (const product of stockProducts) {
+    let uniqueProduct = product.productName;
+    productsName.push(uniqueProduct);
+  }
+  return productsName;
 };
+
+// console.log(getUniqueProductsName());
 
 module.exports = { getUniqueProductsName };
