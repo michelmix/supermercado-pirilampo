d73e2ece2c41573ac32c91e5dd9ffaa2886be4d0
Michel Barbosa <michelbarbosa@outlook.com>
2024-05-02 15:46:00 -0300

    Cria função que eretorna lista de produtos na promoção

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -1,7 +1,18 @@
 const stockProducts = require('./data.json');
 
-const getProductsOnSale = () => {
-  // Desenvolva seu código dentro dessa função...
+const getProductsOnSale = (productName) => {
+  let productsOnSale = [];
+
+  for (const product of stockProducts) {
+    if (product.onSale === true) {
+      productsOnSale.push({
+        description: product.description,
+        formattedPrice: `R$ ${product.price}`,
+        onSale: product.onSale,
+      });
+    }
+  }
+  return productsOnSale;
 };
 
 module.exports = { getProductsOnSale };
