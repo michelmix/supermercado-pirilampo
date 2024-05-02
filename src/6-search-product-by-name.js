01469aea57f454fba495deacd2ea349c6d670844
Michel Barbosa <michelbarbosa@outlook.com>
2024-05-02 15:21:33 -0300

    Cria função para encontrar produto pelo nome

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -1,7 +1,23 @@
 const stockProducts = require('./data.json');
 
-const searchProductByName = () => {
-  // Desenvolva seu código dentro dessa função...
+const searchProductByName = (name) => {
+  let productsInfo = null;
+
+  for (const product of stockProducts) {
+    let productName = product.productName;
+
+    if (!name) {
+      return null;
+    } else if (productName === name) {
+      productsInfo = {
+        description: product.description,
+        formattedPrice: `R$ ${product.price}`,
+      };
+    }
+  }
+  return productsInfo;
 };
+
+console.log(searchProductByName('Feijão'));
 
 module.exports = { searchProductByName };
