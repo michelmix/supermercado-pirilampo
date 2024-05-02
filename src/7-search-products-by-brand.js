37d142a39cb036d393a2c1dc02e4d37661b48054
Michel Barbosa <michelbarbosa@outlook.com>
2024-05-02 15:30:37 -0300

    Cria função para encontrar produto pela marca

--- src/7-search-products-by-brand.js
+++ src/7-search-products-by-brand.js
@@ -1,7 +1,18 @@
 const stockProducts = require('./data.json');
 
-const searchProductsByBrand = () => {
-  // Desenvolva seu código dentro dessa função...
+const searchProductsByBrand = (brand) => {
+  let productByBrand = [];
+
+  for (const product of stockProducts) {
+    if (product.brand === brand) {
+      productByBrand.push({
+        description: product.description,
+        formattedPrice: `R$ ${product.price}`,
+      })      
+    }  
+  }
+  return productByBrand;
+  
 };
 
 module.exports = { searchProductsByBrand };
