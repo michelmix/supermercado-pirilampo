5d47c3a56e62b50e75f4c0d9a7307503c72543e9
Michel Barbosa <michelbarbosa@outlook.com>
2024-05-02 10:40:55 -0300

    Cria função para informar quantidade total de produtos únicos

--- src/1-unique-products-amount.js
+++ src/1-unique-products-amount.js
@@ -1,7 +1,17 @@
 const stockProducts = require('./data.json');
 
 const getUniqueProductsAmount = () => {
-  // Desenvolva seu código dentro dessa função...
+  return stockProducts.length;
 };
+
+// console.log(getUniqueProductsAmount());
+
+
+
+
+
+
+
+
 
 module.exports = { getUniqueProductsAmount };
