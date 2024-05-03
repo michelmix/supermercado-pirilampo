eb9d9713f61a96d6bba844eb7c828ab08672df92
Michel Barbosa <michelbarbosa@outlook.com>
2024-05-03 15:57:26 -0300

    Realiza função que lista todos produtos com vitaminas e cria chave vitaminsInformation para listar as vitaminas

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -1,7 +1,30 @@
 const stockProducts = require('./data.json');
 
 const getProductsRichInVitamin = () => {
-  // Desenvolva seu código dentro dessa função...
+  let productsRichInVitamin = [];
+
+  for (const product of stockProducts) {
+    if (product.nutritionalInfo.vitamins) {
+      const productInfo = {
+        description: product.description,
+        formattedPrice: `R$ ${product.price}`,
+        vitaminsInformation: [],
+      };
+
+      const vitamins = product.nutritionalInfo.vitamins;
+      if (vitamins) {
+        for (const vitamin in vitamins) {
+          productInfo.vitaminsInformation.push(
+            `${vitamin} - ${vitamins[vitamin]}`
+          );
+        }
+      }
+      productsRichInVitamin.push(productInfo);
+    }
+  }
+  return productsRichInVitamin;
 };
+
+console.log(getProductsRichInVitamin());
 
 module.exports = { getProductsRichInVitamin };
