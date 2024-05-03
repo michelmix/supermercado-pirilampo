5810d419761a5da912b5acebb153584558037555
Michel Barbosa <michelbarbosa@outlook.com>
2024-05-03 11:27:54 -0300

    Cria função que retorna produtos e propriedade com restrições alimentares se houver alguma

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -1,7 +1,22 @@
 const stockProducts = require('./data.json');
 
 const getProductsWithAllergyOrIntoleranceInfo = () => {
-  // Desenvolva seu código dentro dessa função...
+  let productsWithRestrictions = [];
+
+  for (product of stockProducts) {
+    const productInfo = {
+      description: product.description,
+      formattedPrice: `R$ ${product.price}`,
+    }
+    if (product["allergyOrIntolerance"]) {
+      productInfo.allergyOrIntoleranceMessage = `Pode conter: ${product.allergyOrIntolerance.join(' ')}`;     
+    }
+    productsWithRestrictions.push(productInfo);
+
+  }
+  return productsWithRestrictions;
 };
+
+console.log(getProductsWithAllergyOrIntoleranceInfo());
 
 module.exports = { getProductsWithAllergyOrIntoleranceInfo };
